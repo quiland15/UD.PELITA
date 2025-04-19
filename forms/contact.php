3ba1de11997057e2220e18b60fd6afa9e4a5fab1
quiland15 <quiland86@gmail.com>
2025-04-19 18:43:26 +0800

    ganti lagi

--- forms/contact.php
+++ forms/contact.php
@@ -2,13 +2,17 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
+// Tampilkan error biar bisa debug
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // Load PHPMailer
 require '../assets/vendor/php-email-form/PHPMailer/PHPMailer.php';
 require '../assets/vendor/php-email-form/PHPMailer/SMTP.php';
 require '../assets/vendor/php-email-form/PHPMailer/Exception.php';
 
 // Ganti email penerima di sini
-$receiving_email_address = 'quiland86@gmail.com';
+$receiving_email_address = 'quiland86@gmail.com'; // Email tujuan penerima
 
 $mail = new PHPMailer(true);
 
@@ -17,26 +21,36 @@ try {
   $mail->isSMTP();
   $mail->Host = 'smtp.gmail.com';
   $mail->SMTPAuth = true;
-  $mail->Username = 'quiland86@gmail.com'; // Email kamu
+  $mail->Username = 'quiland86@gmail.com'; // Ganti dengan email kamu
   $mail->Password = 'dxmpffyrgybpwhvp';  // App password dari Google
   $mail->SMTPSecure = 'tls';
-  $mail->Port = 14167;
+  $mail->Port = 587; // Port SMTP untuk TLS
 
-  // Data dari form
-  $mail->setFrom($_POST['email'], $_POST['name']);
-  $mail->addAddress($receiving_email_address);
-  $mail->Subject = $_POST['subject'];
+  // Ambil data dari form (pastikan POST sudah ada)
+  $name = htmlspecialchars($_POST['name']); // Sanitasi input
+  $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL); // Validasi email
+  $subject = htmlspecialchars($_POST['subject']); // Sanitasi input
+  $message = nl2br(htmlspecialchars($_POST['message'])); // Sanitasi dan preserve newlines
 
-  $body = "From: " . $_POST['name'] . "\n";
-  $body .= "Email: " . $_POST['email'] . "\n";
-  $body .= "Message:\n" . $_POST['message'] . "\n";
+  if (!$email) {
+    throw new Exception('Invalid email address.');
+  }
 
+  // Set pengirim & penerima
+  $mail->setFrom($email, $name);
+  $mail->addAddress($receiving_email_address); // Ganti juga kalau mau kirim ke email lain
+
+  // Isi email
+  $mail->Subject = $subject;
+  $body = "<b>From:</b> $name ($email)<br><b>Message:</b><br>$message";
+  $mail->isHTML(true); // Mengirim email dalam format HTML
   $mail->Body = $body;
 
+  // Kirim email
   $mail->send();
-  echo 'OK'; // Ajax akan membaca ini sebagai sukses
-
+  echo json_encode(['message' => 'Message has been sent successfully!']);
 } catch (Exception $e) {
-  echo 'ERROR: ' . $mail->ErrorInfo;
+  http_response_code(500);
+  echo json_encode(['message' => 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo]);
 }
 ?>
